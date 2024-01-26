c8a6ce2c070cac2c1ef1a9d408a57bb26c4aec33
Danylo Osipchuk <vt222_odi@student.ztu.edu.ua>
2024-01-26 22:09:27 +0200

    add mock console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+double a,
+    b,
+    sum;
+
+Write("Enter a = ");
+double.TryParse(ReadLine(), out a);
+
+Write("Enter b = ");
+double.TryParse(ReadLine(), out b);
+
+sum = a + b;
+WriteLine($"\nSum: {sum}");
